21c429c434741ef6fa6f681299e001b44127338b
doubleground <iolpd42@gmail.com>
2022-10-25 03:53:30 +0900

    mypage 수정2

--- Project/mypage/mypage.js
+++ Project/mypage/mypage.js
@@ -1,47 +1,32 @@
 // 미리보기 이미지 
-    // const file = document.querySelector("input[type='file']");
-
-    // file.addEventListener("change", function(e){
-    //     var reader = new FileReader();
-    //     reader.readAsDataURL(e.target.files[0]);
-    //     reader.onload = function(e){
-    //         let url = e.target.result;
-
-    //         if(url.includes("image")){
-    //             document.querySelector("label div").style.backgroundImage = "url(" + url +")";
-    //         }else{
-    //             document.querySelector("label div").style.backgroundImage = "url(img/no_image.jpg)";
-    //         }
-    //     }
-    // });
-
     
-/*
-현재 보여지는 DB에서의 생일 innerHtml 값
-#now_Birth
+const file = document.querySelector("input[type='file']");
+    
+    console.log(file[0]);
+    file.addEventListener("change", function(e){
+        var reader = new FileReader();
+        reader.readAsDataURL(e.target.files[0]);
+        reader.onload = function(e){
+            let url = e.target.result;
 
-DB로 보낼 생일의 value값
-#chan_Birth
+            if(url.includes("image")){
+                document.querySelector("label div").style.backgroundImage = "url(" + url +")";
+            }else{
+                document.querySelector("label div").style.backgroundImage = "url(img/no_image.jpg)";
+            }
+        }
+    });
 
-현재 보여지는 DB에서의 성별 innerHTML 값
-#now_Gender
-
-DB로 보낼 성별의 value값
-#chan_Gender.values
-
-원래있는 이메일의 placehoder 값
-#email_view630
-입력받는 이메일의 value값
-#email_view630
-
-비밀번호 변경 창으로 바뀌는 버튼
-#chan_password
-
-아이디 연동을 위한 창으로 이동
-#first_id_connect_navar
-// naver
-#first_id_connect_kakao
-// kakao
-.documet_file[type='file']
-
-*/
+    var password = document.getElementById("password")
+    , confirm_password = document.getElementById("confirm_password");
+  
+  function validatePassword(){
+    if(password.value != confirm_password.value) {
+      confirm_password.setCustomValidity("Passwords Don't Match");
+    } else {
+      confirm_password.setCustomValidity(''); // 오류가 없으면 메시지를 빈 문자열로 설정해야한다. 오류 메시지가 비어 있지 않은 한 양식은 유효성 검사를 통과하지 않고 제출되지 않는다.
+    }
+  }
+  
+  password.onchange = validatePassword;
+  confirm_password.onkeyup = validatePassword;
