ec9a71c57bf4fccebbdc6fc74053ef1609d8fe43
devheesukang <heesukang2000@gmail.com>
2022-10-28 06:00:27 +0900

    대외활동 공고 페이지 파일첨부 기능 추가

--- Project/activity/activity_new.js
+++ Project/activity/activity_new.js
@@ -1,9 +1,17 @@
-$(".file").on('change',function(){
-    var fileName = $("#file").val();
-    $(".upload-name").val(fileName);
-  });
-
 function onClickUpload() {
     let myInput = document.getElementById("my-input");
     myInput.click();
 }
+
+function fileUpload() {
+    var fileInput = document.getElementsByClassName("attachfile");
+
+    for (var i = 0; i < fileInput.length; i++) {
+        if (fileInput[i].files.length > 0) {
+            for (var j = 0; j < fileInput[i].files.length; j++) {
+                console.log(fileInput[i].files[j].name); // 파일명 출력
+            }
+        }
+    }
+}
+
