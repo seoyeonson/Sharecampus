1ae25887e42f9272928694afff34d987636f4a81
JS8800gt <JS8800gt@gmail.com>
2022-10-31 17:03:27 +0900

    re

---
+++ sharecampus/src/com/sharecampus/curriculum/controller/CurriculumController.java
@@ -0,0 +1,82 @@
+package package com.sharecampus.curriculum.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import curriculum.command.Command;
+
+/**
+ * Servlet implementation class GetList
+ */
+@WebServlet("*.do")
+public class CurriculumController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	public CurriculumController() {
+		super();
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		actionDo(request, response);
+		
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		actionDo(request, response); 
+	}
+	
+	protected void actionDo(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("UTF-8");
+		
+		Command command = null;	// 어떠한 로직을 수행할지
+		String viewPage = null;	// 어떠한 페이지를 보여줄지
+		
+		// URL로부터 command 분리
+		String uri = request.getRequestURI();
+		String conPath = request.getContextPath();
+		String com = uri.substring(conPath.length());
+		System.out.println(com);
+		switch(com) {
+		case "/curriculum/curriculum.do":
+			command = new ListCommand();
+			command.execute(request, response);
+			viewPage = "curriculum.jsp";
+			break;
+		case "/curriculum/view.do":
+			// TODO
+			break;
+		case "/curriculum/write.do":
+			viewPage = "curriculum_regist.jsp";
+			break;
+		case "/curriculum/writeOk.do":
+//			command = new WriteCommand();
+			command.execute(request, response);
+			break;
+		case "/curriculum/update.do":
+			// TODO
+			break;
+		case "/curriculum/updateOk.do":
+			// TODO
+			break;
+		case "/curriculum/deleteOk.do":
+			// TODO
+			break;
+		}
+		
+		// view page로 forward
+		if (viewPage != null) {
+			RequestDispatcher dispatcher = 
+					request.getRequestDispatcher("/curriculum/" + viewPage);
+			dispatcher.forward(request, response);
+		}
+	}
+
+}
