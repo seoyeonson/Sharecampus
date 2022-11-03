eed345eaa2cfb8777dd8ddd148c14ba4ef147c1a
doubleground <iolpd42@gmail.com>
2022-11-03 19:45:34 +0900

    community 수정 + 오류 수정

--- sharecampus/src/com/sharecampus/community/CommunityFrontController.java
+++ sharecampus/src/com/sharecampus/community/CommunityFrontController.java
@@ -12,11 +12,11 @@ import com.sharecampus.Result;
 public class CommunityFrontController extends HttpServlet{
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-	super.doGet(req, resp);
+		doProcess(req, resp);
 	}
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		super.doPost(req, resp);
+		doProcess(req, resp);
 	}
 	protected void doProcess(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		String target = req.getRequestURI().substring(req.getContextPath().length());
@@ -24,13 +24,9 @@ public class CommunityFrontController extends HttpServlet{
 		
 		if(target.equals("/community/listDs.co")) {
 			result = new CommunitySelectController().execute(req, resp);
-//		커뮤니티 상세글을 DB에서 조회
 		}else if(target.equals("/community/listDi.co")) {
-//		커뮤니티 상세글을 DB에 추가
 		}else if(target.equals("/community/listDd.co")) {
-//		커뮤니티 상세글을 DB에서 삭제
 		}else if(target.equals("/community/listDu.co")) {
-//		커뮤니티 상세글을 DB에서 수정
 		}
 		
 		if(result != null) {
--- sharecampus/src/com/sharecampus/community/CommunitySelectController.java
+++ sharecampus/src/com/sharecampus/community/CommunitySelectController.java
@@ -17,15 +17,10 @@ public class CommunitySelectController extends HttpServlet implements Execute {
 
 	@Override
 	public Result execute(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServerException {
-		CommunityDAO communityDAO = new CommunityDAO();
 		Result result = new Result();
-		String temp = req.getParameter("page");
-		int page = temp == null ? 1 : Integer.parseInt(temp);
-		
-//		한 페이지에 출력되는 게시글의 개수
-//		한 페이지에서 나오는 페이지 버튼의 개수
-		
-		req.setAttribute("community", communityDAO.selectAll());
+		int boardNumber = Integer.valueOf(req.getParameter("boardNumber"));
+		CommunityDAO communityDAO = new CommunityDAO();
+		req.setAttribute("community", communityDAO.select(boardNumber));
 
 		result.setPath("/app/community/community.jsp");
 		return null;
